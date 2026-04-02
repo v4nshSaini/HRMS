fc0702867b2a2f28d06142a3a4a40032b2c13495
palak542 <ps9218787@gmail.com>
2026-04-02 11:18:32 +0530

    Added CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe()); // ✅
-  app.enableCors(); // ✅
+  // ✅ CORS CONFIG (IMPORTANT)
+  app.enableCors({
+    origin: 'http://localhost:3000', // 👈 your frontend URL
+    credentials: true,
+  });
 
-  await app.listen(process.env.PORT ?? 3000); // ✅ LAST
+  await app.listen(5000);
 }
 bootstrap();
